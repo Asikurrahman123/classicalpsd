ab213c690aaba59f535521711bbf9d51aa5f36ae
Asikur Rahman <71285759+Asikurrahman123@users.noreply.github.com>
2023-05-22 01:19:30 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+// BX Slider
+
+$(document).ready(function(){
+    $('.slider').bxSlider();
+    $('.menu_area').meanmenu({
+      meanMenuContainer: '#mobile_menu',
+      meanScreenWidth: 991,
+    });
+  });
